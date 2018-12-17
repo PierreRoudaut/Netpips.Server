8fb876619835c23cb2e6d4d1ad80206e84710ccb
Pierre Roudaut <pierreroudaut@gmail.com>
2018-12-17 15:12:47 +0100

    Removed unused imports

--- Netpips/Core/Model/AppDbContextFactory.cs
+++ Netpips/Core/Model/AppDbContextFactory.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Design;
 using Microsoft.Extensions.Configuration;
 
