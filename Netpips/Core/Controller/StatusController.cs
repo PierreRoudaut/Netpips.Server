15040f06a885f6936bb26149435312394cacf8ce
Pierre Roudaut <pierreroudaut@gmail.com>
2018-12-18 18:10:11 +0100

    Added status route

---
+++ Netpips/Core/Controller/StatusController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Netpips.Core.Controller
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [AllowAnonymous]
+    public class StatusController : ControllerBase
+    {
+        [HttpGet("", Name = "Hello")]
+        [ProducesResponseType(200)]
+        public ObjectResult List()
+        {
+            return Ok(new
+            {
+                Date = DateTime.Now,
+                Ip = HttpContext.Connection.RemoteIpAddress.ToString()
+            });
+        }
+    }
+}
